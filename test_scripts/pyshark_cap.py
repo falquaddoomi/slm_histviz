d2b3dd121d14b4243cce384c16c2cdc4dac9b994
Faisal <falquaddoomi@ucla.edu>
2016-08-18 09:26:07 -0400

    Added SNI sniffing to packet logger example

--- test_scripts/pyshark_cap.py
+++ test_scripts/pyshark_cap.py
@@ -1,8 +1,16 @@
-#!/home/ec2-user/projects/slm_histviz/.venv/bin/python
+#!/bin/env python
 
+import sys
 import pyshark
 
-filtered_cap2 = pyshark.LiveCapture('ppp0', bpf_filter='tcp port 80 or tcp port 443', display_filter='ssl.handshake.extensions_server_name')
+if __name__ == "__main__":
+    # get the arguments from the caller regarding which interface, user, etc. we should be capturing for
 
-for pkt in filtered_cap2.sniff_continuously():
-    print "got packet!"
+    filtered_cap2 = pyshark.LiveCapture('ppp1', bpf_filter='tcp port 80 or tcp port 443', display_filter='ssl.handshake.extensions_server_name')
+
+    for pkt in filtered_cap2.sniff_continuously():
+        print "got packet? %s" % 'ssl!' if hasattr(pkt, 'ssl') else "no info"
+        if hasattr(pkt, 'ssl'):
+            print "SNI: %s" % pkt.ssl.handshake_extensions_server_name if hasattr(pkt.ssl, 'handshake_extensions_server_name') else "no info"
+
+    print "done!"
