6de5e9c0a982b85725e42b2187a930f3dbdbc8c4
falquaddoomi <falquaddoomi@ucla.edu>
2018-01-22 19:17:31 +0000

    Now boot 6 gevent workers instead of 4 sync, and use the gunicorn on the path

--- run_gunicorn.sh
+++ run_gunicorn.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-WORKERS=4
-# WORKER_CLASS="gevent"
-WORKER_CLASS="sync"
+WORKERS=6
+WORKER_CLASS="gevent"
 PORT=5582
 
-cd /home/ec2-user/projects/slm_histviz
-.venv/bin/gunicorn \
---bind 0.0.0.0:${PORT} \
+gunicorn --bind 0.0.0.0:${PORT} \
 -w ${WORKERS} -k ${WORKER_CLASS} \
 --reload wsgi:app --access-logfile -
